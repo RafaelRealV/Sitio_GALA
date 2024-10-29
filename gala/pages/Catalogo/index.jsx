6c5a5d1b8c3159fdab99e9286787395523b28120
RafaelRealV <a20050015@unisierra.edu.mx>
2024-10-29 15:09:56 -0700

    tercera pagina catalogo

--- gala/pages/Catalogo/index.jsx
+++ gala/pages/Catalogo/index.jsx
@@ -1,9 +1,396 @@
-
+import Image from "next/image"
+import styles from "./styles.module.css"
 
 export default function Catalogo() {
   return (
     <div>
-      
+      <div className={styles.divpric}>
+        <div className={styles.card}>
+          <div className={styles.imageContainer}>
+            <Image
+              src="/images/Carneseca.jpeg" // Ruta de tu imagen
+              alt="Producto"
+              width={200}
+              height={200}
+              className={styles.image}
+            />
+          </div>
+          <h3 className={styles.productName}>Canes Secas </h3>
+          <p className={styles.price}>PRICE:</p>
+          <div className={styles.rating}>VALORACIONES: 
+            <Image
+              src="/images/Death_Star.ico"
+              width={16}
+              height={16}
+            />
+            <Image
+              src="/images/Death_Star.ico"
+              width={16}
+              height={16}
+            />
+            <Image
+              src="/images/Death_Star.ico"
+              width={16}
+              height={16}
+            />
+            <Image
+              src="/images/Death_Star.ico"
+              width={16}
+              height={16}
+            />
+            <Image
+              src="/images/Death_Star.ico"
+              width={16}
+              height={16}
+            />
+
+          </div>
+          <div className={styles.buttons}>
+            <button className={styles.button}>carrito</button>
+            <button className={styles.button}>informacion</button>
+
+          </div>
+        </div>
+
+        <div className={styles.card}>
+          <div className={styles.imageContainer}>
+            <Image
+              src="/images/Carneseca.jpeg" // Ruta de tu imagen
+              alt="Producto"
+              width={200}
+              height={200}
+              className={styles.image}
+            />
+          </div>
+          <h3 className={styles.productName}>Canes Secas </h3>
+          <p className={styles.price}>PRICE:</p>
+          <div className={styles.rating}>VALORACIONES: 
+            <Image
+              src="/images/Death_Star.ico"
+              width={16}
+              height={16}
+            />
+            <Image
+              src="/images/Death_Star.ico"
+              width={16}
+              height={16}
+            />
+            <Image
+              src="/images/Death_Star.ico"
+              width={16}
+              height={16}
+            />
+            <Image
+              src="/images/Death_Star.ico"
+              width={16}
+              height={16}
+            />
+            <Image
+              src="/images/Death_Star.ico"
+              width={16}
+              height={16}
+            />
+
+          </div>
+          <div className={styles.buttons}>
+            <button className={styles.button}>carrito</button>
+            <button className={styles.button}>informacion</button>
+
+          </div>
+        </div>
+        
+
+        <div className={styles.card}>
+          <div className={styles.imageContainer}>
+            <Image
+              src="/images/Carneseca.jpeg" // Ruta de tu imagen
+              alt="Producto"
+              width={200}
+              height={200}
+              className={styles.image}
+            />
+          </div>
+          <h3 className={styles.productName}>Canes Secas </h3>
+          <p className={styles.price}>PRICE:</p>
+          <div className={styles.rating}>VALORACIONES: 
+            <Image
+              src="/images/Death_Star.ico"
+              width={16}
+              height={16}
+            />
+            <Image
+              src="/images/Death_Star.ico"
+              width={16}
+              height={16}
+            />
+            <Image
+              src="/images/Death_Star.ico"
+              width={16}
+              height={16}
+            />
+            <Image
+              src="/images/Death_Star.ico"
+              width={16}
+              height={16}
+            />
+            <Image
+              src="/images/Death_Star.ico"
+              width={16}
+              height={16}
+            />
+
+          </div>
+          <div className={styles.buttons}>
+            <button className={styles.button}>carrito</button>
+            <button className={styles.button}>informacion</button>
+
+          </div>
+        </div>
+
+        <div className={styles.card}>
+          <div className={styles.imageContainer}>
+            <Image
+              src="/images/Carneseca.jpeg" // Ruta de tu imagen
+              alt="Producto"
+              width={200}
+              height={200}
+              className={styles.image}
+            />
+          </div>
+          <h3 className={styles.productName}>Canes Secas </h3>
+          <p className={styles.price}>PRICE:</p>
+          <div className={styles.rating}>VALORACIONES: 
+            <Image
+              src="/images/Death_Star.ico"
+              width={16}
+              height={16}
+            />
+            <Image
+              src="/images/Death_Star.ico"
+              width={16}
+              height={16}
+            />
+            <Image
+              src="/images/Death_Star.ico"
+              width={16}
+              height={16}
+            />
+            <Image
+              src="/images/Death_Star.ico"
+              width={16}
+              height={16}
+            />
+            <Image
+              src="/images/Death_Star.ico"
+              width={16}
+              height={16}
+            />
+
+          </div>
+          <div className={styles.buttons}>
+            <button className={styles.button}>carrito</button>
+            <button className={styles.button}>informacion</button>
+
+          </div>
+        </div>
+      </div>
+
+
+
+
+
+
+      <div className={styles.divpric}>
+        <div className={styles.card}>
+          <div className={styles.imageContainer}>
+            <Image
+              src="/images/Carneseca.jpeg" // Ruta de tu imagen
+              alt="Producto"
+              width={200}
+              height={200}
+              className={styles.image}
+            />
+          </div>
+          <h3 className={styles.productName}>Canes Secas </h3>
+          <p className={styles.price}>PRICE:</p>
+          <div className={styles.rating}>VALORACIONES: 
+            <Image
+              src="/images/Death_Star.ico"
+              width={16}
+              height={16}
+            />
+            <Image
+              src="/images/Death_Star.ico"
+              width={16}
+              height={16}
+            />
+            <Image
+              src="/images/Death_Star.ico"
+              width={16}
+              height={16}
+            />
+            <Image
+              src="/images/Death_Star.ico"
+              width={16}
+              height={16}
+            />
+            <Image
+              src="/images/Death_Star.ico"
+              width={16}
+              height={16}
+            />
+
+          </div>
+          <div className={styles.buttons}>
+            <button className={styles.button}>carrito</button>
+            <button className={styles.button}>informacion</button>
+
+          </div>
+        </div>
+
+        <div className={styles.card}>
+          <div className={styles.imageContainer}>
+            <Image
+              src="/images/Carneseca.jpeg" // Ruta de tu imagen
+              alt="Producto"
+              width={200}
+              height={200}
+              className={styles.image}
+            />
+          </div>
+          <h3 className={styles.productName}>Canes Secas </h3>
+          <p className={styles.price}>PRICE:</p>
+          <div className={styles.rating}>VALORACIONES: 
+            <Image
+              src="/images/Death_Star.ico"
+              width={16}
+              height={16}
+            />
+            <Image
+              src="/images/Death_Star.ico"
+              width={16}
+              height={16}
+            />
+            <Image
+              src="/images/Death_Star.ico"
+              width={16}
+              height={16}
+            />
+            <Image
+              src="/images/Death_Star.ico"
+              width={16}
+              height={16}
+            />
+            <Image
+              src="/images/Death_Star.ico"
+              width={16}
+              height={16}
+            />
+
+          </div>
+          <div className={styles.buttons}>
+            <button className={styles.button}>carrito</button>
+            <button className={styles.button}>informacion</button>
+
+          </div>
+        </div>
+        
+
+        <div className={styles.card}>
+          <div className={styles.imageContainer}>
+            <Image
+              src="/images/Carneseca.jpeg" // Ruta de tu imagen
+              alt="Producto"
+              width={200}
+              height={200}
+              className={styles.image}
+            />
+          </div>
+          <h3 className={styles.productName}>Canes Secas </h3>
+          <p className={styles.price}>PRICE:</p>
+          <div className={styles.rating}>VALORACIONES: 
+            <Image
+              src="/images/Death_Star.ico"
+              width={16}
+              height={16}
+            />
+            <Image
+              src="/images/Death_Star.ico"
+              width={16}
+              height={16}
+            />
+            <Image
+              src="/images/Death_Star.ico"
+              width={16}
+              height={16}
+            />
+            <Image
+              src="/images/Death_Star.ico"
+              width={16}
+              height={16}
+            />
+            <Image
+              src="/images/Death_Star.ico"
+              width={16}
+              height={16}
+            />
+
+          </div>
+          <div className={styles.buttons}>
+            <button className={styles.button}>carrito</button>
+            <button className={styles.button}>informacion</button>
+
+          </div>
+        </div>
+
+        <div className={styles.card}>
+          <div className={styles.imageContainer}>
+            <Image
+              src="/images/Carneseca.jpeg" // Ruta de tu imagen
+              alt="Producto"
+              width={200}
+              height={200}
+              className={styles.image}
+            />
+          </div>
+          <h3 className={styles.productName}>Canes Secas </h3>
+          <p className={styles.price}>PRICE:</p>
+          <div className={styles.rating}>VALORACIONES: 
+            <Image
+              src="/images/Death_Star.ico"
+              width={16}
+              height={16}
+            />
+            <Image
+              src="/images/Death_Star.ico"
+              width={16}
+              height={16}
+            />
+            <Image
+              src="/images/Death_Star.ico"
+              width={16}
+              height={16}
+            />
+            <Image
+              src="/images/Death_Star.ico"
+              width={16}
+              height={16}
+            />
+            <Image
+              src="/images/Death_Star.ico"
+              width={16}
+              height={16}
+            />
+
+          </div>
+          <div className={styles.buttons}>
+            <button className={styles.button}>carrito</button>
+            <button className={styles.button}>informacion</button>
+
+          </div>
+        </div>
+      </div>
+
     </div>
   )
 }
